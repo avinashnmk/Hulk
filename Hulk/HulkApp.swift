39c87deca70c52cf0f46ca1d97194cbad90d5382
AvinashKandaswamy <Avinash.Kandaswamy@target.com>
2022-03-06 15:57:59 -0500

    final

--- Hulk/HulkApp.swift
+++ Hulk/HulkApp.swift
@@ -12,28 +12,52 @@ struct HulkApp: App {
     
     //   @State private var scrums = DailyScrum.sampleData
     @StateObject private var store = ScrumStore()
+    @State private var errorWrapper: ErrorWrapper?
     var body: some Scene {
         WindowGroup{
             NavigationView {
-                ScrumsView(scrums: $store.scrums){
-                    ScrumStore.save(scrums: store.scrums) { result in
-                        if case .failure(let error) = result {
-                                                  fatalError(error.localizedDescription)
-                                              }
-                    }
-                }
+                
+                ScrumsView(scrums: $store.scrums) {
+                                  Task {
+                                      do {
+                                          try await ScrumStore.save(scrums: store.scrums)
+                                      } catch  {
+                                          fatalError("Error saving scrums.")
+                                      }
+                                  }
+                              }
+//                ScrumsView(scrums: $store.scrums){
+//                    ScrumStore.save(scrums: store.scrums) { result in
+//                        if case .failure(let error) = result {
+//                                                  fatalError(error.localizedDescription)
+//                                              }
+//                    }
+                
             }
-            .onAppear {
-                ScrumStore.load { result in
-                    switch result {
-                    case .failure(let error):
-                        fatalError(error.localizedDescription)
-                    case .success(let scrums):
-                        store.scrums = scrums
-                    }
-                }
-            }
-            
+            .task {
+                            do {
+                                store.scrums = try await ScrumStore.load()
+                            } catch {
+                                errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
+
+                            }
+                        }
+            .sheet(item: $errorWrapper, onDismiss: {
+                store.scrums = DailyScrum.sampleData
+                      }) { wrapper in
+                          ErrorView(errorWrapper: wrapper)
+                      }
+//            .onAppear {
+//                ScrumStore.load { result in
+//                    switch result {
+//                    case .failure(let error):
+//                        fatalError(error.localizedDescription)
+//                    case .success(let scrums):
+//                        store.scrums = scrums
+//                    }
+//                }
+//            }
+//
         }
     }
 }
--- Hulk/ScrumStore.swift
+++ Hulk/ScrumStore.swift
@@ -15,6 +15,23 @@ class ScrumStore: ObservableObject {
         try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
             .appendingPathComponent("scrums.data")
     }
+    static func load() async throws -> [DailyScrum] {
+        try await withCheckedThrowingContinuation { continuation in
+            load { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                    
+                case .success(let scrums):
+                    continuation.resume(returning: scrums)
+                }
+                
+            }
+            
+        }
+    }
+    
+    
     
     static func load(completion: @escaping (Result<[DailyScrum],Error>)->Void){
         DispatchQueue.global(qos: .background).async {
@@ -38,20 +55,35 @@ class ScrumStore: ObservableObject {
             
         }
     }
+    
+    @discardableResult
+     static func save(scrums: [DailyScrum]) async throws -> Int {
+         try await withCheckedThrowingContinuation { continuation in
+             save(scrums: scrums) { result in
+                 switch result {
+                               case .failure(let error):
+                                   continuation.resume(throwing: error)
+                               case .success(let scrumsSaved):
+                                   continuation.resume(returning: scrumsSaved)
+                               }
+                       }
+                }
+     }
+    
     static func save(scrums: [DailyScrum], completion: @escaping (Result<Int, Error>)->Void) {
         DispatchQueue.global(qos: .background).async {
-                do {
-                    let data = try JSONEncoder().encode(scrums)
-                    let outfile = try fileURL()
-                    try data.write(to: outfile)
-                    DispatchQueue.main.async {
-                                    completion(.success(scrums.count))
-                                }
-                } catch {
-                    DispatchQueue.main.async {
-                                      completion(.failure(error))
-                                  }
+            do {
+                let data = try JSONEncoder().encode(scrums)
+                let outfile = try fileURL()
+                try data.write(to: outfile)
+                DispatchQueue.main.async {
+                    completion(.success(scrums.count))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
                 }
             }
+        }
     }
 }
